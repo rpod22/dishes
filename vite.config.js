f93f87d42f0119820beb0a776efdefd2f79ec1c3
Radosław Podgórski <rpod002@gmail.com>
2023-05-07 22:41:40 +0200

    fifth

--- vite.config.js
+++ vite.config.js
@@ -3,15 +3,20 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
-module.exports = {
-  // ...
   build: {
-    rollupOptions: {
-      input: {
-        main: './public/index.html'
-      }
-    }
+    outDir: 'dist',
+    assetsDir: 'assets',
+    emptyOutDir: true
+  },
+  base: '/dishes1/',
+  mode: 'production',
+  optimizeDeps: {
+    include: [
+      'react',
+      'react-dom',
+      'react-router-dom',
+      'react-scripts',
+      '@fortawesome/fontawesome-free/css/all.css'
+    ]
   }
-}
+});
